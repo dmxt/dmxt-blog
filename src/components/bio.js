2364b2a324a28e662d221b3770b1c0cbb9fe4942
Dana <dana@dmxt.org>
2022-09-21 18:16:11 -0400

    Change logo

--- src/components/bio.js
+++ src/components/bio.js
@@ -37,7 +37,7 @@ const Bio = () => {
         className="bio-avatar"
         layout="fixed"
         formats={["auto", "webp", "avif"]}
-        src="../images/profile-pic.png"
+        src="../images/logo.png"
         width={50}
         height={50}
         quality={95}
