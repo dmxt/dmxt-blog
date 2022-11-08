94547e62ae8ec701a2fe020f5490e4ba25302e88
Dana <dana@dmxt.org>
2022-11-07 19:54:09 -0500

    Update 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,16 +1,28 @@
 import * as React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+
+const email = "dana@dmxt.org"
 
 const NotFoundPage = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
 
   return (
     <Layout location={location} title={siteTitle}>
-      <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <h1>404: Not Found!</h1>
+      <p>You just hit a link that doesn&#39;t exist... the sadness. 😢</p>
+      <p>
+        Go back to{" "}
+        <Link to="/">
+          <span>homepage</span>
+        </Link>
+      </p>
+      <p>
+        If you found this link from somewhere, please report this error to my
+        email: <a href="mailto:dana@dmxt.org">{email}</a> so I can fix it.
+      </p>
     </Layout>
   )
 }
