3db80deedf9e141149581864073b4963a8a770ba
Dana <dana@dmxt.org>
2023-01-31 11:35:57 -0500

    Add footer link "← all blogs" in article

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,7 +5,9 @@ const Layout = ({ location, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
   let header
+  let footer
 
+  // Header
   if (isRootPath) {
     header = (
       <h1 className="main-heading">
@@ -17,8 +19,35 @@ const Layout = ({ location, children }) => {
   } else {
     header = (
       <Link className="header-link-home" to="/">
-        ← home
+        ← all blogs
       </Link>
+    )
+  }
+
+  // Footer
+  if (isRootPath) {
+    footer = (
+      <div className="main-footer">
+        <p>
+        © {new Date().getFullYear()} dmxt
+        {`. `}
+        Powered by <a href="https://www.gatsbyjs.com">Gatsby</a>
+        </p>
+      </div>
+    )
+  } else {
+    footer = (
+      <div className="article-footer">
+        <p>
+        © {new Date().getFullYear()} dmxt
+        {`. `}
+        Powered by <a href="https://www.gatsbyjs.com">Gatsby</a>
+        </p>
+
+        <Link className="header-link-home" to="/">
+          ← all blogs
+        </Link>
+      </div>
     )
   }
 
@@ -26,11 +55,7 @@ const Layout = ({ location, children }) => {
     <div className="global-wrapper" data-is-root-path={isRootPath}>
       <header className="global-header">{header}</header>
       <main>{children}</main>
-      <footer>
-        © {new Date().getFullYear()} dmxt
-        {`. `}
-        Powered by <a href="https://www.gatsbyjs.com">Gatsby</a>
-      </footer>
+      <footer className="footer">{footer}</footer>
     </div>
   )
 }
